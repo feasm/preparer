99f5d49e6ff5d99d96549995ede3aefdafc306ee
Alexandre Abreu <alexandre.oliveira.abreu>
2019-06-10 17:12:04 -0300

    Added test target

---
+++ Nano3Tests/Nano3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Nano3Tests.swift
+//  Nano3Tests
+//
+//  Created by Felipe Alexander Silva Melo on 10/06/19.
+//  Copyright © 2019 Felipe Melo. All rights reserved.
+//
+
+import XCTest
+
+class Nano3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
